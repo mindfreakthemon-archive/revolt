75c030d3db80663ed7c05729e88f60815ac21b99
mindfreakthemon <mfthemon@gmail.com>
2014-05-25 22:37:46 +0300

    Mah Nigga's files

---
+++ Gruntfile.js
@@ -0,0 +1,120 @@
+var path = require('path');
+
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		jshint: {
+			main: [
+				'Gruntfile.js',
+				'server.js',
+				'server/**/*.js'
+			]
+		},
+
+		connect: {
+			main: {
+				options: {
+					port: 80,
+					keepalive: true,
+					base: '.'
+				}
+			}
+		},
+
+		nodemon: {
+			server: {
+				script: 'server.js',
+				options: {
+					env: {
+						PORT: '80'
+					},
+					ignore: ['node_modules/**'],
+					watch: [
+						'server.js',
+						'server/**/*.js'
+					]
+				}
+			}
+		},
+
+		clean: ['static/css', 'static/js/templates.js'],
+
+		jade: {
+			templates: {
+				files: {
+					'static/js/templates.js': 'src/jade/**/*.jade'
+				},
+				options: {
+					amd: true,
+					client: true,
+					processName: function (name) {
+						return path.basename(name, '.jade');
+					}
+				}
+			}
+		},
+
+		stylus: {
+			css: {
+				expand: true,
+				cwd: 'src/styles',
+				src: ['**/*.styl'],
+				dest: 'static/css',
+				ext: '.css'
+			}
+		},
+
+		watch: {
+			jshint: {
+				files: [
+					'Gruntfile.js',
+					'server.js',
+					'server/**/*.js'
+				],
+				tasks: ['jshint:main']
+			},
+
+			templates: {
+				files: ['src/jade/**/*.jade'],
+				tasks: ['jade:templates'],
+				options: {
+					atBegin: true
+				}
+			},
+
+			stylus: {
+				files: ['src/styles/**/*.styl'],
+				tasks: ['stylus:css'],
+				options: {
+					atBegin: true
+				}
+			},
+
+			livereload: {
+				files: ['src/**', 'server/views/**'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		concurrent: {
+			watch: {
+				tasks: ['watch:templates', 'watch:less', 'watch:livereload']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-nodemon');
+
+	grunt.registerTask('test', 'jshint');
+	grunt.registerTask('default', ['concurrent:watch']);
+};
