f1bb5eeaec278ce6dc62023ac1b15ae130c51a09
Vitalii Rakovtsii <vitalii.rakovtsii@playtech.com>
2015-09-26 00:29:18 +0300

    Fixing gulpfile and some requirements

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,12 @@ require('require-dir')('./tasks');
 gulp.task('assets', ['bower', 'tpl', 'jade', 'stylus', 'js']);
 
 gulp.task('watch', ['assets'], function () {
-	gulp.watch(['./app/tpl/**/*.jade'], ['tpl']);
-	gulp.watch(['./app/jade/**/*.jade'], ['jade']);
-	gulp.watch(['./app/stylus/**/*.styl'], ['stylus']);
-	gulp.watch(['./app/js/**/*.{es6,js}'], ['js']);
-	gulp.watch(['./app/**/*'], ['manifest']);
+	gulp.watch(['./client/app/tpl/**/*.jade'], ['tpl']);
+	gulp.watch(['./client/app/jade/**/*.jade'], ['jade']);
+	gulp.watch(['./client/app/styl/**/*.styl'], ['stylus']);
+	gulp.watch(['./client/app/js/**/*.{es6,js}'], ['js']);
+	gulp.watch(['./client/app/**/*'], ['manifest']);
+	gulp.run('nodemon');
 });
 
 gulp.task('default', ['connect', 'watch']);
